4dae77a3fb3d185c7139a3dbbd862c53fb3a5e47
lujie <lujie@yuantutech.com>
2022-04-15 16:10:22 +0800

    feat: add router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
-import ReduxDemo from '../pages/redux';
+import Link from 'next/link';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', function () {
+  Array.from(document.getElementsByClassName('line')).forEach((dom) =>
+    dom.removeAttribute('style')
+  );
+});
+
 export default function Index() {
   return (
     <div>
-      <ReduxDemo />
+      <Link href='/redux'>
+        <button>redux demo</button>
+      </Link>
+      <Link href='/mobx'>
+        <button>mobx demo</button>
+      </Link>
     </div>
   );
 }
